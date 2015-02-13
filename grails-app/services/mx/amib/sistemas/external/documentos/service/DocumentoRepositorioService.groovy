441d6063f2948de8a1a58a41d3b9e1098e735ff2
gabriel-medina <gabriel.m.g.245245@gmail.com>
2015-02-13 14:38:24 -0600

    DEV
    desc: se integra nuevo metodo para busqueda por UUID en DocumentoRepositorioService

--- grails-app/services/mx/amib/sistemas/external/documentos/service/DocumentoRepositorioService.groovy
+++ grails-app/services/mx/amib/sistemas/external/documentos/service/DocumentoRepositorioService.groovy
@@ -3,6 +3,7 @@ package mx.amib.sistemas.external.documentos.service
 import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.Date
+import java.util.ArrayList
 
 import mx.amib.sistemas.util.service.ArchivoTO
 import mx.amib.sistemas.util.service.ArchivoTemporalService
@@ -18,8 +19,6 @@ import grails.converters.JSON
 import grails.plugins.rest.client.RestBuilder
 import grails.transaction.Transactional
 
-
-
 /**
  * DocumentoRepositorioService
  *
@@ -27,8 +26,8 @@ import grails.transaction.Transactional
  * HTTP/REST al sistema de amibDocumentos
  *
  * @author Gabriel
- * @version 1.2 - (Última actualización) 03/02/2015
- *			1.1 - (Última actualización) 22/09/2014
+ * @version 1.2 - (Última actualización) 11/02/2015 - Se añaden métodos obtenerTodosPorMatricula,
+ *			1.1 - 22/09/2014
  */
 @Transactional
 class DocumentoRepositorioService {
@@ -40,6 +39,19 @@ class DocumentoRepositorioService {
 	String documentoPoderUpdateUrl
 	String documentoRevocacionSaveUrl
 	String documentoRevocacionUpdateUrl
+	
+	String findAllByMatriculaUrl
+	String findAllLikeNombreArchivoUrl
+	String findAllLikeDescripcionUrl
+	String findAllGenericUrl
+	String findAllByTypeUrl
+	
+	String listDocumentoUrl
+	String listCnbvDgaOficioUrl
+	String listDocumentoPoderUrl
+	String listFotoSustentanteUrl
+	String listDocumentoSustentanteUrl
+	String listRevocacionUrl
 	
 	String saveMultipartUrl
 	String getUrl
@@ -62,7 +74,6 @@ class DocumentoRepositorioService {
 		def restMultipart = new RestBuilder()
 		
 		def resp = rest.get(restUrl)
-		resp.json instanceof JSONObject
 		
 		if(resp.json == null)
 			return null
@@ -95,7 +106,7 @@ class DocumentoRepositorioService {
 					docRep.tipoDocumentoRespaldo = resp.json.'tipoDocumentoRespaldo'
 					docRep.representanteLegalNombreCompleto = resp.json.'representanteLegalNombreCompleto'
 					docRep.numeroEscritura = resp.json.'numeroEscritura'
-					docRep.fechaApoderamiento = df.parse(resp.json.'fechaApoderamiento'.substring(0,10))
+					docRep.fechaRevocacion = df.parse(resp.json.'fechaRevocacion'.substring(0,10))
 					docRep.matriculasRevocados = resp.json.'matriculasRevocados'
 					docRep.nombresRevocados = resp.json.'nombresRevocados'
 					docRep.notario = resp.json.'notario'
@@ -123,6 +134,177 @@ class DocumentoRepositorioService {
 			//docRep.fechaCreacion = resp.json.'fechaCreacion'
 		}
 		return docRep
+	}
+	
+	DocumentoRepositorioTO obtenerMetadatosDocumentoNew(String uuid){
+		def docRep = null
+		String restUrl = getUrl + uuid
+		
+		def rest = new RestBuilder()
+		def resp = rest.get(restUrl)
+		
+		if(resp.json != null && resp.json instanceof JSONObject)
+			docRep = this.llenarMetadatosJsonDocumento(resp.json)
+			
+		return docRep
+	}
+	
+	/**
+	 * Obtiene, del repositorio amibDocumentos,
+	 * los metadatos de todos documentos.
+	 *
+	 * @return Coleccion de objetos DocumentoRepositorioTO
+	 *
+	 */
+	Collection<DocumentoRepositorioTO> obtenerTodos(Integer max = 10, Integer offset = 0, String sort = "id", String order = "desc"){
+		Collection<DocumentoRepositorioTO> resultCollection = new ArrayList<DocumentoRepositorioTO>()
+		String restUrl = listDocumentoUrl + "?max=" + max + "&offset=" + offset + "&sort=" + sort + "&order=" + order
+		def rest = new RestBuilder()
+		
+		//obtiene los datos de servicio REST
+		def resp = rest.get(restUrl)
+		if(resp.json != null && resp.json instanceof JSONObject){
+			def count = resp.json.'count'
+			if(count>0) {
+				def jsonArrayList = resp.json.'list'
+				jsonArrayList.each{	x ->
+					DocumentoRepositorioTO doc = this.llenarMetadatosJsonDocumento(x)
+					if(doc != null)
+						resultCollection.add(doc)
+				}
+			}
+		}
+		
+		return resultCollection
+	}
+	
+	/**
+	 * Obtiene, del repositorio amibDocumentos,
+	 * los metadatos de todos documentos, dado el
+	 * tipo del documento solicitado
+	 *
+	 * @param cd
+	 * @return Coleccion de objetos DocumentoRepositorioTO
+	 *
+	 */
+	Collection<DocumentoRepositorioTO> obtenerTodos(ClaseDocumento cd, Integer max = 10, Integer offset = 0, String sort = "id", String order = "desc"){
+		Collection<DocumentoRepositorioTO> resultCollection = new ArrayList<DocumentoRepositorioTO>()
+		String restUrl = findAllByTypeUrl + "?idTipo=" + cd.getId() + "&max=" + max + "&offset=" + offset + "&sort=" + sort + "&order=" + order
+		def rest = new RestBuilder()
+		
+		println "URL -> " + restUrl
+		
+		//obtiene los datos de servicio REST
+		def resp = rest.get(restUrl)
+		if(resp.json != null && resp.json instanceof JSONObject){
+			def count = resp.json.'count'
+			if(count>0) {
+				def jsonArrayList = resp.json.'list'
+				jsonArrayList.each{	x ->
+					DocumentoRepositorioTO doc = this.llenarMetadatosJsonDocumento(x)
+					if(doc != null)
+						resultCollection.add(doc)
+				}
+			}
+		}
+		
+		return resultCollection
+	}
+	
+	/**
+	 * Obtiene, del repositorio amibDocumentos, 
+	 * los metadatos de todos documentos que 
+	 * coincidan con el número de matrícula
+	 * proporcionado
+	 *
+	 * @param numeroMatricula
+	 * @return Coleccion de objetos DocumentoRepositorioTO
+	 *
+	 */
+	Collection<DocumentoRepositorioTO> obtenerTodosPorMatricula(Integer numeroMatricula, Integer max = 10, Integer offset = 0, String sort = "id", String order = "desc"){
+		Collection<DocumentoRepositorioTO> resultCollection = new ArrayList<DocumentoRepositorioTO>()
+		String restUrl = findAllByMatriculaUrl + "?matricula=" + numeroMatricula + "&max=" + max + "&offset=" + offset + "&sort=" + sort + "&order=" + order
+		def rest = new RestBuilder()
+		
+		//obtiene los datos de servicio REST
+		def resp = rest.get(restUrl)
+		if(resp.json != null && resp.json instanceof JSONObject){
+			def count = resp.json.'count'
+			if(count>0) {
+				def jsonArrayList = resp.json.'list'
+				jsonArrayList.each{	x ->
+					DocumentoRepositorioTO doc = this.llenarMetadatosJsonDocumento(x)
+					if(doc != null)
+						resultCollection.add(doc)
+				}
+			}
+		}
+		
+		return resultCollection
+	}
+	
+	/**
+	 * Obtiene, del repositorio amibDocumentos,
+	 * los metadatos de todos documentos que
+	 * coincidan con alguna porción del nombre de archivo
+	 * proporcionado, sin considerar mayus/minus
+	 *
+	 * @param nombreArchivo
+	 * @return Coleccion de objetos DocumentoRepositorioTO
+	 *
+	 */
+	Collection<DocumentoRepositorioTO> obtenerTodosPorNombreArchivoILike(String nombreArchivo, Integer max = 10, Integer offset = 0, String sort = "id", String order = "desc"){
+		Collection<DocumentoRepositorioTO> resultCollection = new ArrayList<DocumentoRepositorioTO>()
+		String restUrl = findAllLikeNombreArchivoUrl + "?nombreArchivo=" + nombreArchivo + "&max=" + max + "&offset=" + offset + "&sort=" + sort + "&order=" + order
+		def rest = new RestBuilder()
+		
+		//obtiene los datos de servicio REST
+		def resp = rest.get(restUrl)
+		if(resp.json != null && resp.json instanceof JSONObject){
+			def count = resp.json.'count'
+			if(count>0) {
+				def jsonArrayList = resp.json.'list'
+				jsonArrayList.each{	x ->
+					DocumentoRepositorioTO doc = this.llenarMetadatosJsonDocumento(x)
+					if(doc != null)
+						resultCollection.add(doc)
+				}
+			}
+		}
+		
+		return resultCollection
+	}
+	
+	/**
+	 * Obtiene, del repositorio amibDocumentos,
+	 * los metadatos de todos documentos que
+	 * coincidan con alguna porción de alguna descripción
+	 * proporcionada, sin considerar mayus/minus
+	 *
+	 * @param descripcion
+	 * @return Coleccion de objetos DocumentoRepositorioTO
+	 *
+	 */
+	Collection<DocumentoRepositorioTO> obtenerTodosPorDescripcionILike(String descripcion, Integer max = 10, Integer offset = 0, String sort = "id", String order = "desc"){
+		Collection<DocumentoRepositorioTO> resultCollection = new ArrayList<DocumentoRepositorioTO>()
+		String restUrl = findAllLikeDescripcionUrl + "?descripcion=" + descripcion + "&max=" + max + "&offset=" + offset + "&sort=" + sort + "&order=" + order
+		def rest = new RestBuilder()
+		
+		//obtiene los datos de servicio REST
+		def resp = rest.get(restUrl)
+		if(resp.json != null && resp.json instanceof JSONObject){
+			def count = resp.json.'count'
+			if(count>0) {
+				def jsonArrayList = resp.json.'list'
+				jsonArrayList.each{	x ->
+					DocumentoRepositorioTO doc = this.llenarMetadatosJsonDocumento(x)
+					if(doc != null)
+						resultCollection.add(doc)
+				}
+			}
+		}
+		
+		return resultCollection
 	}
 	
 	/**
@@ -275,6 +457,74 @@ class DocumentoRepositorioService {
 		}
 		return dr
 	}
+	
+	private DocumentoRepositorioTO llenarMetadatosJsonDocumento(def jsonObjectDoc){
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd")
+		DateFormat dftm = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+		DocumentoRepositorioTO doc = null	
+		def claseDocumento = ClaseDocumento.fromInteger(jsonObjectDoc.'idTipo')
+		switch(claseDocumento){
+			case ClaseDocumento.DOC_SUSTENTANTE:
+				doc = new DocumentoSustentanteRepositorioTO()
+				def docIns = (DocumentoSustentanteRepositorioTO)doc
+				docIns.numeroMatricula = jsonObjectDoc.'numeroMatricula'
+				docIns.tipoDocumentoSustentante = jsonObjectDoc.'tipoDocumentoSustentante'
+				docIns.nombreCompleto = jsonObjectDoc.'nombreCompleto'
+			break;
+			case ClaseDocumento.DOCUMENTO:
+				doc = new DocumentoRepositorioTO()
+			break;
+			case ClaseDocumento.FOTO_SUSTENTANTE:
+				doc = new DocumentoFotoSustentanteRepositorioTO()
+				def docIns = (DocumentoFotoSustentanteRepositorioTO)doc
+				docIns.numeroMatricula = jsonObjectDoc.'numeroMatricula'
+				docIns.nombreCompleto = jsonObjectDoc.'nombreCompleto'
+			break;
+			case ClaseDocumento.OFICIO_CNBV:
+				doc = new DocumentoOficioCnbvRespositorioTO()
+				def docIns = (DocumentoOficioCnbvRespositorioTO)doc
+				docIns.matriculas = jsonObjectDoc.'matriculas'
+				docIns.nombres = jsonObjectDoc.'nombres'
+				docIns.autorizaciones = jsonObjectDoc.'autorizaciones'
+			break;
+			case ClaseDocumento.PODER:
+				doc = new DocumentoPoderRepositorioTO()
+				def docIns = (DocumentoPoderRepositorioTO)doc
+				docIns.tipoDocumentoRespaldo = jsonObjectDoc.'tipoDocumentoRespaldo'
+				docIns.representanteLegalNombreCompleto = jsonObjectDoc.'representanteLegalNombreCompleto'
+				docIns.numeroEscritura = jsonObjectDoc.'numeroEscritura'
+				docIns.fechaApoderamiento = df.parse(jsonObjectDoc.'fechaApoderamiento'.substring(0,10))
+				docIns.matriculasApoderados = jsonObjectDoc.'matriculasApoderados'
+				docIns.nombresApoderados = jsonObjectDoc.'nombresApoderados'
+				docIns.notario = jsonObjectDoc.'notario'
+				docIns.grupoFinanciero = jsonObjectDoc.'grupoFinanciero'
+				docIns.institucion = jsonObjectDoc.'institucion'
+			break;
+			case ClaseDocumento.REVOCACION:
+				doc = new DocumentoRevocacionRepositorioTO()
+				def docIns = (DocumentoRevocacionRepositorioTO)doc
+				docIns.tipoDocumentoRespaldo = jsonObjectDoc.'tipoDocumentoRespaldo'
+				docIns.representanteLegalNombreCompleto = jsonObjectDoc.'representanteLegalNombreCompleto'
+				docIns.numeroEscritura = jsonObjectDoc.'numeroEscritura'
+				docIns.fechaRevocacion = df.parse(jsonObjectDoc.'fechaRevocacion'.substring(0,10))
+				docIns.matriculasRevocados = jsonObjectDoc.'matriculasRevocados'
+				docIns.nombresRevocados = jsonObjectDoc.'nombresRevocados'
+				docIns.notario = jsonObjectDoc.'notario'
+				docIns.grupoFinanciero = jsonObjectDoc.'grupoFinanciero'
+				docIns.institucion = jsonObjectDoc.'institucion'
+			break;
+		}
+		if(doc!=null) {
+			doc.id = jsonObjectDoc.'id'
+			doc.uuid = jsonObjectDoc.'uuid'
+			doc.clave = jsonObjectDoc.'clave'
+			doc.nombre = jsonObjectDoc.'nombre'
+			doc.mimetype = jsonObjectDoc.'mimetype'
+			doc.fechaCreacion = dftm.parse(jsonObjectDoc.'fechaCreacion'.substring(0,19) + "-0000")
+			doc.fechaModificacion = dftm.parse(jsonObjectDoc.'fechaModificacion'.substring(0,19) + "-0000")
+		}
+		return doc
+	}
 }
 
 class DocumentoRepositorioTO{
@@ -328,6 +578,39 @@ class DocumentoSustentanteRepositorioTO extends DocumentoRepositorioTO{
 	String nombreCompleto
 }
 
-enum ClaseDocumento{
-	DOCUMENTO, OFICIO_CNBV, PODER, REVOCACION, FOTO_SUSTENTANTE, DOC_SUSTENTANTE
+public enum ClaseDocumento{
+	DOCUMENTO(1,"DOCUMENTO"), OFICIO_CNBV(2,"OFICIO_CNBV"), PODER(3,"PODER"), 
+	FOTO_SUSTENTANTE(4,"FOTO_SUSTENTANTE"), DOC_SUSTENTANTE(5,"DOC_SUSTENTANTE"), REVOCACION(6,"REVOCACION")
+	
+	private Integer id
+	private String name
+	private static Map<Integer,ClaseDocumento> _map = null
+	
+	public ClaseDocumento(Integer id, String name){
+		this.id = id
+		this.name = name
+	}
+	
+	public Integer getId()
+	{
+		return this.id
+	}
+	
+	private static synchronized Map<Integer,ClaseDocumento> getInstanceMap(){
+		if(_map == null)
+		{
+			_map = new HashMap<Integer,ClaseDocumento>()
+			_map.put(ClaseDocumento.DOCUMENTO.id, ClaseDocumento.DOCUMENTO)
+			_map.put(ClaseDocumento.OFICIO_CNBV.id, ClaseDocumento.OFICIO_CNBV)
+			_map.put(ClaseDocumento.PODER.id, ClaseDocumento.PODER)
+			_map.put(ClaseDocumento.FOTO_SUSTENTANTE.id, ClaseDocumento.FOTO_SUSTENTANTE)
+			_map.put(ClaseDocumento.DOC_SUSTENTANTE.id, ClaseDocumento.DOC_SUSTENTANTE)
+			_map.put(ClaseDocumento.REVOCACION.id, ClaseDocumento.REVOCACION)
+		}
+		return _map
+	}
+	
+	public static ClaseDocumento fromInteger(Integer x){
+		return ClaseDocumento.getInstanceMap().get(x)
+	}
 }

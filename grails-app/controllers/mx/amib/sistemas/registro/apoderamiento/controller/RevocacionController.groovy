becf3ee5c4ad561bbacf2127dbef64dd0b0febc8
gabriel-medina <gabriel.m.g.245245@gmail.com>
2015-08-22 21:23:03 -0500

    DEV
    desc: checkpoint

--- grails-app/controllers/mx/amib/sistemas/registro/apoderamiento/controller/RevocacionController.groovy
+++ grails-app/controllers/mx/amib/sistemas/registro/apoderamiento/controller/RevocacionController.groovy
@@ -1,10 +1,56 @@
 package mx.amib.sistemas.registro.apoderamiento.controller
 
+import grails.converters.JSON
+import mx.amib.sistemas.external.catalogos.service.NotarioTO
+
 class RevocacionController {
 
+	def notarioService
+	
     def index() { }
 	
 	def create() { }
 	
 	def show() { }
+	
+	
+	def findNotarioByNumeroNotariaAndIdEntidadFederativa(){
+		int numeroNotario = params.int('numeroNotario')
+		long idEntidadFederativa = params.long('idEntidadFederativa')
+		
+		List<NotarioResult> resObj = null
+		Map<String,Object> res = new HashMap<String,Object>()
+		
+		resObj = NotarioResult.copyFromServicesResults( notarioService.findAllBy(100, 0, "id", "asc", idEntidadFederativa, numeroNotario, "").list )
+		
+		res.put('status', 'OK')
+		res.put('object', resObj )
+		
+		render(res as JSON)
+	}
+	
+	static class NotarioResult{
+		long id
+		String text
+		
+		public static List<NotarioResult> copyFromServicesResults(List<NotarioTO> notarios){
+			List<NotarioResult> lres = new ArrayList<NotarioResult>()
+			
+			lres.add( (new NotarioResult()).setViewNullValues() )
+			notarios.each { x ->
+				NotarioResult nr = new NotarioResult()
+				nr.id = x.id
+				nr.text = x.nombreCompleto
+				lres.add(nr)
+			}
+			
+			return lres
+		}
+		
+		public NotarioResult setViewNullValues(){
+			this.id = -1
+			this.text = '-Seleccione-'
+			return this
+		}
+	}
 }
--- web-app/js/mx.amib.sistemas.registro.autorizacionCnbv.revocacion.form.notario.js
+++ web-app/js/mx.amib.sistemas.registro.autorizacionCnbv.revocacion.form.notario.js
@@ -4,13 +4,80 @@ app.Notario = Backbone.Model.extend ({
 	defaults: {
 		numeroNotaria: '',
 		idEntidadFederativa: -1,
-		entidadesFederativas: new Array(),
+		entidadesFederativas: [ { id:'-1',text:'-Seleccione-' } ] ,
 		
-		notariosEncontrados: new Array(),
-		idNotarioSeleccionado: -1
+		notariosEncontrados: [ { id:'-1',text:'-Seleccione-' } ],
+		idNotarioSeleccionado: -1,
+		
+		//flags de estado
+		processing: false,
+		
+		//errores de "valdidación" al momento de buscar el notario
+		errorNumeroNotariaInvalidType: false,
+		errorNumeroNotariaBlank: false,
+		errorEntidadFederativaNonSelected: false,
+		
+		//errores al no encontrar notario
+		errorNotarioNotFound: false,
+		
+		//error al no seleccionar ningun notario
+		errorNotarioNotSelected: false,
+		
+		//url para obtener al notario
+		findNotarioByNumeroNotariaAndIdEntidadFederativaUrl: ''
+	},
+	setBlankNotarioEncontrados: function(){
+		this.set('notariosEncontrados',[ { id:'-1',text:'-Seleccione-' } ]);
+	},
+	findNotarioByNumeroNotariaAndIdEntidadFederativa: function(numeroNotario,idEntidadFederativa){
+		//llamada ajax para actualizar a los notarios
+		$.ajax({
+			url: _this.get('findNotarioByNumeroNotariaAndIdEntidadFederativaUrl'),
+			beforeSend: function( xhr ){
+				_this.set('processing',true);
+				_this.set('errorNumeroNotariaInvalidType',false);
+				_this.set('errorNumeroNotariaBlank',false);
+				_this.set('errorEntidadFederativaNonSelected',false);
+				_this.set('errorNotarioNotFound',false);
+				_this.set('errorNotarioNotSelected',false);
+			},
+			type: 'GET',
+			data: { numeroNotario:numeroNotario, idEntidadFederativa:idEntidadFederativa },
+		}).done( function( data ) {
+			_this.set('processing',false);
+			if(data.status == "OK"){
+				_this.set('notariosEncontrados',data.object);
+			}
+			else{
+				_this.set('errorNotarioNotFound',true);
+			}
+		});
 	}
 });
 
 app.NotarioView =  Backbone.View.extend({
+	el: '#divNotario',
+	template: _.template( $('#notarioTemplate').html() ),
 	
+	initialize: function(options){
+		this.render();
+		
+		this.listenTo(this.model, 'change:numeroNotaria', this.invalidateEntry );
+		this.listenTo(this.model, 'change:entidadesFederativas', this.invalidateEntry );
+		
+		this.listenTo(this.model, 'change:notariosEncontrados', this.renderNotariosEncontrados );
+		
+		this.listenTo(this.model, 'change:processing', this.renderProcessing );
+		
+		this.listenTo(this.model, 'change:errorNumeroNotariaInvalidType', this.renderError );
+		this.listenTo(this.model, 'change:errorNumeroNotariaBlank', this.renderError );
+		this.listenTo(this.model, 'change:errorEntidadFederativaNonSelected', this.renderError );
+		this.listenTo(this.model, 'change:errorNotarioNotFound', this.renderError );
+		this.listenTo(this.model, 'change:errorNotarioNotSelected', this.renderError );
+	},
+	
+	//al haber cualquier cambio en el numero de Notario y/o entidad Federativa
+	invalidateEntry: function(){
+		
+	},
 });
